a8a414b0c7faa6121323a9eb7d2a3e90e6416ab1
hackds87833-creator <hackds87833@gmail.com>
2026-05-20 22:58:53 -0500

    Add files via upload

---
+++ config (1).js
@@ -0,0 +1,19 @@
+/**
+ * StreamFusion — config.js (generado 2026-05-21T01:05:38.417Z)
+ * Sube este archivo a la RAÍZ de tu sitio (junto a index.html)
+ */
+window.__APP_CONFIG__ = {
+  firebase: {
+    apiKey: "AIzaSyDBPfSfS19V7ofCV92LXlRX4t67L_9ch3o",
+    authDomain: "pelisplex-52d2c.firebaseapp.com",
+    databaseURL: "",
+    projectId: "pelisplex-52d2c",
+    storageBucket: "pelisplex-52d2c.firebasestorage.app",
+    messagingSenderId: "1049646716563",
+    appId: "1:1049646716563:web:9259e4023aedb66912df66",
+    measurementId: "",
+  },
+  tmdb: {
+    apiKey: "cc5b94165972aa509a349161d13d4fc9",
+  },
+};
